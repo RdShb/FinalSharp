b8b76a6cb9a8800423a1c8d88de07ba4de5ddcb0
Lautaro Rocca <lrocca2019@gmail.com>
2023-05-08 17:11:57 +0200

    Update Anagrama.cs

--- Anagrama.cs
+++ Anagrama.cs
@@ -2,24 +2,32 @@ class Anagram
 {
     public void Anagrama()
     {
-        string palabra1 = "roma";
-        string palabra2 = "amor";
-
-        char[] letras1 = palabra1.ToLower().ToCharArray();
-        char[] letras2 = palabra2.ToLower().ToCharArray();
-
-        Array.Sort(letras1);
-        Array.Sort(letras2);
-
-        bool sonAnagramas = letras1.SequenceEqual(letras2);
-
-        if (sonAnagramas)
+        Console.WriteLine("Escriba una palabra");
+        string? palabra1 = Console.ReadLine();
+        Console.WriteLine("Escriba otra palabra");
+        string? palabra2 = Console.ReadLine();
+        if (String.IsNullOrEmpty(palabra1) || String.IsNullOrEmpty(palabra2))
         {
-            Console.WriteLine(palabra1 + " y " + palabra2 + " son anagramas.");
+            Console.WriteLine("Una de las palabras introducidas no es valida");
         }
         else
         {
-            Console.WriteLine(palabra1 + " y " + palabra2 + " no son anagramas.");
+            char[] letras1 = palabra1.ToLower().ToCharArray();
+            char[] letras2 = palabra2.ToLower().ToCharArray();
+
+            Array.Sort(letras1);
+            Array.Sort(letras2);
+
+            bool sonAnagramas = letras1.SequenceEqual(letras2);
+
+            if (sonAnagramas)
+            {
+                Console.WriteLine(palabra1 + " y " + palabra2 + " son anagramas.");
+            }
+            else
+            {
+                Console.WriteLine(palabra1 + " y " + palabra2 + " no son anagramas.");
+            }
         }
     }
 }
